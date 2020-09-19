43d0d1888c1891f7c524039e584f71fedd228891
Sergey Grigorenko <miraigajettolab@gmail.com>
2020-09-19 19:06:33 +0300

    Add modal window

--- src/App.js
+++ src/App.js
@@ -1,21 +1,97 @@
-import React from 'react';
+import React, {useState} from 'react';
 import connect from '@vkontakte/vkui-connect';
-import { View } from '@vkontakte/vkui';
+import { View, ModalRoot, ModalPage, ModalPageHeader, PanelHeaderButton, FormLayout, Textarea, Button, Input, platform,  ANDROID, IOS} from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
-
+import Icon24Cancel from '@vkontakte/icons/dist/24/cancel';
+import Icon24Done from '@vkontakte/icons/dist/24/done';
 import Home from './panels/Home';
 import PrimaryForm from './panels/PrimaryForm'
 import Kiara from './panels/Kiara';
 
+const MODAL_PAGE_FILTERS = 'filters';
+const osname = platform();
+
 class App extends React.Component {
 	constructor(props) {
 		super(props);
-
 		this.state = {
 			activePanel: 'primaryForm',
 			fetchedUser: null,
+			activeModal: null,
+			modalHistory: [],
+			expArray: [],
+			position: "",
+			company: "",
+			description: "",
+			startMonth: "",
+			finishMonth: ""
 		};
+
+		this.changeHandler = this.changeHandler.bind(this)
+		this.modalBack = () => {
+			this.setActiveModal(this.state.modalHistory[this.state.modalHistory.length - 2]);
+		  };
 	}
+
+	changeHandler(event) {
+		const name = event.target.name
+		const value = event.target.value
+		const type = event.target.type
+		const checked = event.target.checked
+		type === "checkbox" ? this.setState({[name]:checked}) : this.setState({[name]:value})
+	}
+
+	addExp = (obj) => {
+		let expArrayOld = this.state.expArray;
+		let expArray = 	[...expArrayOld, {
+            "id": expArrayOld.length, 
+            "position": obj.position,
+            "company": obj.company,
+            "description": obj.description,
+            "startMonth": obj.startMonth,
+            "finishMonth": obj.finishMonth
+        }]
+		
+		this.setState({
+			expArray
+		});
+
+		this.setState({
+			position: "",
+			company: "",
+			description: "",
+			startMonth: "",
+			finishMonth: ""
+		});
+
+		this.modalBack()
+    }
+
+    removeExp = id => {
+		let expArrayOld = this.state.expArray;
+		let expArray = expArrayOld.filter(item => item.id !== id)
+		this.setState({
+			expArray
+		});
+    }
+
+	setActiveModal(activeModal) {
+		activeModal = activeModal || null;
+		let modalHistory = this.state.modalHistory ? [...this.state.modalHistory] : [];
+	
+		if (activeModal === null) {
+		  modalHistory = [];
+		} else if (modalHistory.indexOf(activeModal) !== -1) {
+		  modalHistory = modalHistory.splice(0, modalHistory.indexOf(activeModal) + 1);
+		} else {
+		  modalHistory.push(activeModal);
+		}
+	
+		this.setState({
+		  activeModal,
+		  modalHistory
+		});
+	  };
 
 	componentDidMount() {
 		connect.subscribe((e) => {
@@ -34,11 +110,88 @@ class App extends React.Component {
 		this.setState({ activePanel: e.currentTarget.dataset.to })
 	};
 
+	greeting() {
+		console.log('hello!');
+		console.log("hahah")
+	}
+
 	render() {
+
+		const modal = (
+			 <ModalRoot
+			  activeModal={this.state.activeModal}
+			  onClose={this.modalBack}
+			>
+			  <ModalPage
+				id={MODAL_PAGE_FILTERS}
+				onClose={this.modalBack}
+				header={
+				  <ModalPageHeader
+					left={osname === ANDROID && <PanelHeaderButton onClick={this.modalBack}><Icon24Cancel /></PanelHeaderButton>}
+					right={<PanelHeaderButton onClick={this.modalBack}>{osname === IOS ? 'Отмена' : <Icon24Done />}</PanelHeaderButton>}
+				  >
+					Добавить опыт
+				  </ModalPageHeader>
+				}
+			  >
+         <FormLayout>
+			<Input 
+				top="Название позиции в организации" 
+				onChange = {this.changeHandler}
+				name = "position"
+				value = {this.state.position}
+				placeholder="Например официант" />
+			<Input 
+				top={"Название организации"}
+				onChange = {this.changeHandler}
+				name = "company"
+				value = {this.state.company}
+				placeholder={"Я работал в  . . ."} />
+			<Textarea 
+				top="Описание работы" 
+				onChange = {this.changeHandler}
+				name = "description"
+				value = {this.state.description}
+				placeholder="В мои обязанности входило  . . ."/>
+			<Input 
+				top={"Начало работы"}
+				type="month"
+				onChange = {this.changeHandler}
+				name = "startMonth"
+				value = {this.state.startMonth} />
+			<Input 
+				top={"Конец работы"}
+				type="month"
+				onChange = {this.changeHandler}
+				name = "finishMonth"
+				value = {this.state.finishMonth}/>
+				<Button size="xl" onClick={e => {this.addExp(
+					{
+						"position": this.state.position,
+						"company": this.state.company,
+						"description": this.state.description,
+						"startMonth": this.state.startMonth,
+						"finishMonth": this.state.finishMonth	
+					}
+				
+				)}}
+				disabled={!(
+					this.state.position.length > 0 &&
+					this.state.company.length > 0 &&
+					this.state.description.length > 0 &&
+					this.state.startMonth.length > 0 &&
+					this.state.finishMonth.length > 0
+				)}
+				>Добавить</Button>
+		</FormLayout>
+			  </ModalPage>
+			</ModalRoot>
+		);
+
 		return (
-			<View activePanel={this.state.activePanel}>
+			<View activePanel={this.state.activePanel} modal={modal}>
 				<Home id="home" go={this.go} />
-				<PrimaryForm id="primaryForm" go={this.go} />
+				<PrimaryForm id="primaryForm" go={this.go} modal={() => this.setActiveModal(MODAL_PAGE_FILTERS)} expArray={this.state.expArray} removeExp={this.removeExp}/>
 				<Kiara id="kiara" go={this.go} />
 			</View>
 		);
--- src/panels/PrimaryForm.js
+++ src/panels/PrimaryForm.js
@@ -9,31 +9,6 @@ import Icon28CancelOutline from '@vkontakte/icons/dist/28/cancel_outline';
 const osname = platform();
 
 function PrimaryForm(props){
-    const [position, setPosition] = useState("");
-    const [company, setCompany] = useState(""); 
-    const [description, setDescription] = useState("");
-    const [startMonth, setStartMonth] = useState("");
-    const [finishMonth, setFinishMonth] = useState("");
-    const [expArray, setExpArray] = useState([])
-
-    const addExp = () => {
-        setExpArray([...expArray, {
-            "id": expArray.length, 
-            "position": position,
-            "company": company,
-            "description": description,
-            "startMonth": startMonth,
-            "finishMonth": finishMonth
-        }])
-        setPosition("");
-        setCompany("");
-        setDescription("");
-        setStartMonth("");
-        setFinishMonth("");
-    }
-
-    console.log(expArray);
-
     return <Panel id={props.id}>
 		<PanelHeader
             separator={false}
@@ -48,7 +23,7 @@ function PrimaryForm(props){
         <CardGrid>
 
         {
-        expArray.map((item) => (
+        props.expArray.map((item) => (
         <Card size="l">
             <RichCell
                 disabled
@@ -56,7 +31,7 @@ function PrimaryForm(props){
                 text={<div className="companyText">{item.company}</div>}
                 caption={item.startMonth + "   " + item.finishMonth}
                 after= {<div className="cardButtons">
-                    <Icon28CancelOutline className="Icon28Cancel"/>
+                    <Icon28CancelOutline className="Icon28Cancel" onClick = {() => {props.removeExp(item.id)}}/>
                     <Icon28EditOutline className="Icon28Edit"/>
                 </div>}
                 bottom={item.description}
@@ -66,44 +41,18 @@ function PrimaryForm(props){
         </Card>))
         }
         </CardGrid>
+        <Button className="modalButton" size="xl" onClick={props.modal}>Добавить</Button>
         </Group>
 
-        <FormLayout>
-
-            <Input 
-                top="Название позиции в организации" 
-                onChange = {e => setPosition(e.target.value)}
-                value = {position}
-                placeholder="Например официант" />
-            <Input 
-                top={"Название организации"}
-                onChange = {e => setCompany(e.target.value)}
-                value = {company}
-                placeholder={"Я работал в  . . ."} />
-            <Textarea 
-                top="Описание работы" 
-                onChange = {e => setDescription(e.target.value)}
-                value = {description}
-                placeholder="В мои обязанности входило  . . ."/>
-            <Input 
-                top={"Начало работы"}
-                type="month"
-                onChange = {e => setStartMonth(e.target.value)}
-                value = {startMonth} />
-            <Input 
-                top={"Конец работы"}
-                type="month"
-                onChange = {e => setFinishMonth(e.target.value)}
-                value = {finishMonth}/>
-                <Button size="xl" onClick={addExp}>Добавить</Button>
-          </FormLayout>
 	</Panel>
 };
 
 PrimaryForm.propTypes = {
 	id: PropTypes.string.isRequired,
     go: PropTypes.func.isRequired,
-    targeted: PropTypes.bool.isRequired
+    modal: PropTypes.func.isRequired,
+    expArray: PropTypes.array.isRequired,
+    removeExp: PropTypes.func.isRequired
 };
 
 export default PrimaryForm;
